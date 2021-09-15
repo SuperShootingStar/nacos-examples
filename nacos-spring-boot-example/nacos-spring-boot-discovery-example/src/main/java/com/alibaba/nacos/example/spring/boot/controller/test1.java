b017b8ff8af455e5cd7154d7d7d87985565ef2e5
SuperShootingStar <1332760156@qq.com>
2021-09-15 13:54:38 +0800

    first_test

---
+++ nacos-spring-boot-example/nacos-spring-boot-discovery-example/src/main/java/com/alibaba/nacos/example/spring/boot/controller/test1.java
@@ -0,0 +1,14 @@
+package com.alibaba.nacos.example.spring.boot.controller;
+
+/**
+ * 测试
+ * @author mhgao4
+ *
+ */
+public class test1 {
+
+	public static void main(String[] args) {
+		System.out.println("ceshi");
+	}
+	
+}
