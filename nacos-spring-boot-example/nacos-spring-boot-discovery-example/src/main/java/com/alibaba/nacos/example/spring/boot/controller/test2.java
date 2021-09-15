0f6bf692c6bd0296b849d1d5a2e0477d0e82cc2d
SuperShootingStar <1332760156@qq.com>
2021-09-15 14:03:39 +0800

    four_commit

---
+++ nacos-spring-boot-example/nacos-spring-boot-discovery-example/src/main/java/com/alibaba/nacos/example/spring/boot/controller/test2.java
@@ -0,0 +1,19 @@
+package com.alibaba.nacos.example.spring.boot.controller;
+
+/**
+ * 测试
+ * @author mhgao4
+ *
+ */
+public class test2 {
+
+	public static void main(String[] args) {
+		test();
+	}
+	
+	public static void test() {
+		System.out.println("测试1");
+		System.out.println("测试2");
+	}
+	
+}
